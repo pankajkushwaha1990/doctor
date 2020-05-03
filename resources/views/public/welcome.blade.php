16a86cc9d8729c4945600f35733a6f743ffa42b8
pankajkushwaha1990 <pankajkushwaha1990@gmail.com>
2020-05-03 17:41:05 +0530

    services added

--- resources/views/public/welcome.blade.php
+++ resources/views/public/welcome.blade.php
@@ -197,35 +197,35 @@
                         </div>
                         <div class="col-md-7">
                             <div class="section-header">    
-                                <h2 class="mt-2">Availabe Features in Our Clinic</h2>
+                                <h2 class="mt-2">Availabe Features</h2>
                                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
                             </div>  
                             <div class="features-slider slider">
                                 <!-- Slider Item -->
                                 <div class="feature-item text-center">
                                     <img src="{{asset('template/')}}/assets/img/features/feature-01.jpg" class="img-fluid" alt="Feature">
-                                    <p>Patient Ward</p>
+                                    <p>Patient Services</p>
                                 </div>
                                 <!-- /Slider Item -->
                                 
                                 <!-- Slider Item -->
                                 <div class="feature-item text-center">
                                     <img src="{{asset('template/')}}/assets/img/features/feature-02.jpg" class="img-fluid" alt="Feature">
-                                    <p>Test Room</p>
+                                    <p>Medicine Services</p>
                                 </div>
                                 <!-- /Slider Item -->
                                 
                                 <!-- Slider Item -->
                                 <div class="feature-item text-center">
                                     <img src="{{asset('template/')}}/assets/img/features/feature-03.jpg" class="img-fluid" alt="Feature">
-                                    <p>ICU</p>
+                                    <p>Cashless Hospitalization</p>
                                 </div>
                                 <!-- /Slider Item -->
                                 
                                 <!-- Slider Item -->
                                 <div class="feature-item text-center">
                                     <img src="{{asset('template/')}}/assets/img/features/feature-04.jpg" class="img-fluid" alt="Feature">
-                                    <p>Laboratory</p>
+                                    <p>Pathlogy Services</p>
                                 </div>
                                 <!-- /Slider Item -->
                                 
@@ -239,7 +239,7 @@
                                 <!-- Slider Item -->
                                 <div class="feature-item text-center">
                                     <img src="{{asset('template/')}}/assets/img/features/feature-06.jpg" class="img-fluid" alt="Feature">
-                                    <p>Medical</p>
+                                    <p>Doctor </p>
                                 </div>
                                 <!-- /Slider Item -->
                             </div>
