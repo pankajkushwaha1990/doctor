747f950a09d30f7a3b955c28f77351d95f82b8b2
pankajkushwaha1990 <pankajkushwaha1990@gmail.com>
2020-05-03 17:43:52 +0530

    file added

--- resources/views/public/welcome.blade.php
+++ resources/views/public/welcome.blade.php
@@ -230,10 +230,10 @@
                                 <!-- /Slider Item -->
                                 
                                 <!-- Slider Item -->
-                                <div class="feature-item text-center">
+                               <!--  <div class="feature-item text-center">
                                     <img src="{{asset('template/')}}/assets/img/features/feature-05.jpg" class="img-fluid" alt="Feature">
                                     <p>Operation</p>
-                                </div>
+                                </div> -->
                                 <!-- /Slider Item -->
                                 
                                 <!-- Slider Item -->
@@ -250,115 +250,7 @@
             <!-- /Availabe Features -->
             
             <!-- Blog Section -->
-           <section class="section section-blogs">
-                <div class="container-fluid">
-                
-                    <!-- Section Header -->
-                    <div class="section-header text-center">
-                        <h2>Blogs and News</h2>
-                        <p class="sub-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                    <!-- /Section Header -->
-                    
-                    <div class="row blog-grid-row">
-                        <div class="col-md-6 col-lg-3 col-sm-12">
-                        
-                            <!-- Blog Post -->
-                            <div class="blog grid-blog">
-                                <div class="blog-image">
-                                    <a href="blog-details.html"><img class="img-fluid" src="{{asset('template/')}}/assets/img/blog/blog-01.jpg" alt="Post Image"></a>
-                                </div>
-                                <div class="blog-content">
-                                    <ul class="entry-meta meta-item">
-                                        <li>
-                                            <div class="post-author">
-                                                <a href="doctor-profile.html"><img src="{{asset('template/')}}/assets/img/doctors/doctor-thumb-01.jpg" alt="Post Author"> <span>Dr. Ruby Perrin</span></a>
-                                            </div>
-                                        </li>
-                                        <li><i class="far fa-clock"></i> 4 Dec 2019</li>
-                                    </ul>
-                                    <h3 class="blog-title"><a href="blog-details.html">Doccure – Making your clinic painless visit?</a></h3>
-                                    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur em adipiscing elit, sed do eiusmod tempor.</p>
-                                </div>
-                            </div>
-                            <!-- /Blog Post -->
-                            
-                        </div>
-                        <div class="col-md-6 col-lg-3 col-sm-12">
-                        
-                            <!-- Blog Post -->
-                            <div class="blog grid-blog">
-                                <div class="blog-image">
-                                    <a href="blog-details.html"><img class="img-fluid" src="{{asset('template/')}}/assets/img/blog/blog-02.jpg" alt="Post Image"></a>
-                                </div>
-                                <div class="blog-content">
-                                    <ul class="entry-meta meta-item">
-                                        <li>
-                                            <div class="post-author">
-                                                <a href="doctor-profile.html"><img src="{{asset('template/')}}/assets/img/doctors/doctor-thumb-02.jpg" alt="Post Author"> <span>Dr. Darren Elder</span></a>
-                                            </div>
-                                        </li>
-                                        <li><i class="far fa-clock"></i> 3 Dec 2019</li>
-                                    </ul>
-                                    <h3 class="blog-title"><a href="blog-details.html">What are the benefits of Online Doctor Booking?</a></h3>
-                                    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur em adipiscing elit, sed do eiusmod tempor.</p>
-                                </div>
-                            </div>
-                            <!-- /Blog Post -->
-                            
-                        </div>
-                        <div class="col-md-6 col-lg-3 col-sm-12">
-                        
-                            <!-- Blog Post -->
-                            <div class="blog grid-blog">
-                                <div class="blog-image">
-                                    <a href="blog-details.html"><img class="img-fluid" src="{{asset('template/')}}/assets/img/blog/blog-03.jpg" alt="Post Image"></a>
-                                </div>
-                                <div class="blog-content">
-                                    <ul class="entry-meta meta-item">
-                                        <li>
-                                            <div class="post-author">
-                                                <a href="doctor-profile.html"><img src="{{asset('template/')}}/assets/img/doctors/doctor-thumb-03.jpg" alt="Post Author"> <span>Dr. Deborah Angel</span></a>
-                                            </div>
-                                        </li>
-                                        <li><i class="far fa-clock"></i> 3 Dec 2019</li>
-                                    </ul>
-                                    <h3 class="blog-title"><a href="blog-details.html">Benefits of consulting with an Online Doctor</a></h3>
-                                    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur em adipiscing elit, sed do eiusmod tempor.</p>
-                                </div>
-                            </div>
-                            <!-- /Blog Post -->
-                            
-                        </div>
-                        <div class="col-md-6 col-lg-3 col-sm-12">
-                        
-                            <!-- Blog Post -->
-                            <div class="blog grid-blog">
-                                <div class="blog-image">
-                                    <a href="blog-details.html"><img class="img-fluid" src="{{asset('template/')}}/assets/img/blog/blog-04.jpg" alt="Post Image"></a>
-                                </div>
-                                <div class="blog-content">
-                                    <ul class="entry-meta meta-item">
-                                        <li>
-                                            <div class="post-author">
-                                                <a href="doctor-profile.html"><img src="{{asset('template/')}}/assets/img/doctors/doctor-thumb-04.jpg" alt="Post Author"> <span>Dr. Sofia Brient</span></a>
-                                            </div>
-                                        </li>
-                                        <li><i class="far fa-clock"></i> 2 Dec 2019</li>
-                                    </ul>
-                                    <h3 class="blog-title"><a href="blog-details.html">5 Great reasons to use an Online Doctor</a></h3>
-                                    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur em adipiscing elit, sed do eiusmod tempor.</p>
-                                </div>
-                            </div>
-                            <!-- /Blog Post -->
-                            
-                        </div>
-                    </div>
-                    <div class="view-all text-center"> 
-                        <a href="blog-list.html" class="btn btn-primary">View All</a>
-                    </div>
-                </div>
-            </section>
+
             <!-- /Blog Section -->          
             
             <!-- Footer -->
